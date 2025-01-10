7826468825e7957f2dce9845d8c4c0b09bb4054b
Uchechukwu <uchechukwu@Uchechukwus-MacBook-Air-4.local>
2025-01-10 20:29:10 +0200

    blog full functionality. able to edit and delete blog posts

--- index.js
+++ index.js
@@ -20,19 +20,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.render("index.ejs", { userData: userArray }); //remember to show the user data only if user data has been entered into form in your index.ejs
 });
+
 app.get("/home", (req, res) => {
   res.redirect("/");
 });
 
+app.get("/delete/article/:slug", (req, res) => {
+  const deleteSlug = req.params.slug;
+  userArray.pop((anyPost) => anyPost.slug === deleteSlug);
+  res.redirect("/");
+  res.render("index.ejs", { userData: userArray });
+});
+
+app.get("/edit/article/:slug", (req, res) => {
+  console.log(userArray);
+  const editSlug = req.params.slug;
+  const fetchPost = userArray.find((anyPost) => anyPost.slug === editSlug);
+  userArray.pop((anyPost) => anyPost.slug === editSlug);
+  res.render("editblog.ejs", { blog: fetchPost });
+  res.redirect("/");
+});
 app.get("/article/:slug", (req, res) => {
   //that :slug is just a placeholder for whatever slug is clicked (which would be an anchor tag in the index.ejs file)
   const postSlug = req.params.slug;
   const fetchedPost = userArray.find(
     (fetchedPost) => fetchedPost.slug === postSlug
   );
-  {
-    post: fetchedPost;
-  }
 
   res.render("article.ejs", { post: fetchedPost });
 });
@@ -41,6 +54,7 @@ app.post("/submit", (req, res) => {
   const slug = customSlug(req.body.titleofblog);
   const newPost = { ...req.body, slug };
   userArray.push(newPost);
+  console.log(userArray);
 
   res.redirect("/"); //after user posts info in the form, redirect user back to the homepage which is index.ejs
 });
